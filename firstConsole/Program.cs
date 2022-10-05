59a7e8170fb3e3821ca7f49315f98d3b89f2bc2f
DDDev <DDDev@DESKTOP-MO5HLC4>
2022-10-05 19:22:19 +0300

    Add project files.

---
+++ firstConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace firstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // byte 0 to 255
+            
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            //Short
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+            //ushort
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(ushort.MinValue);
+            //int
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            //uint
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(uint.MinValue);
+            //long
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+            //ulong
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine(ulong.MinValue);
+
+            Console.WriteLine("vamo espana");
+        }
+    }
+}
